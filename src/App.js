28400bd77ad5e912bdb5d298a807ad468e49d843
Elmar Amanov <Elmar.amanov10@gmail.com>
2021-03-25 04:32:41 +0400

    Data Binding React

--- src/App.js
+++ src/App.js
@@ -13,16 +13,20 @@ var minusOneHandler = () => {
     console.log("minus one");
 }
 
-
-
-function trick(){
-    var element = (
-        <>
-            <h1>Tarix : {new Date().toLocaleTimeString()}</h1>
-        </>
+function renderApp(){
+    var template = (
+            <div>
+                <h1 id="header">number:  {number} </h1>
+                <button onClick={ addOneHandler }> + </button>
+                <button onClick={ minusOneHandler }> - </button>
+            </div>
     );
+    ReactDOM.render(template , root);
+}
+function trick(){
+    var element = ( <> <h1>Tarix : {new Date().toLocaleTimeString()}</h1> </> );
     ReactDOM.render(element , root);
 }
 setInterval(()=>{trick();},1000)
 
-
+//renderApp();
